62cdbcc878df243e25d846858595f8837f97207e
Jonathan Vieira <j.vieira.ti@gmail.com>
2021-02-26 18:21:41 -0300

    [add] - development and configuration of ChallengesContext
    ChallengesContext configuration and application in components

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,24 +1,28 @@
+
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
-  const hasActiveChallege = true;
+  const {activeChallenge, resetChallenge} = useContext(ChallengesContext)
 
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallege ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg"/>
+            <img src={`icons/${activeChallenge.type}.svg`}/>
             <strong>Novo desafio</strong>
-            <p>Levante e beba agua carai!!</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button
               type="button"
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >Falhei</button>
 
             <button
